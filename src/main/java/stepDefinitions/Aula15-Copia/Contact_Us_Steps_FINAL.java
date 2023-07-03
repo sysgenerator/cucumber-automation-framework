11dfa10cb09c8e31917ab7d1fc8e3fee35bf11c2
Heredium Search <sysgenerator@gmail.com>
2023-07-03 22:39:38 +0100

    Finalizado. Funcionando!

--- src/main/java/stepDefinitions/Aula15-Copia/Contact_Us_Steps_FINAL.java
+++ src/main/java/stepDefinitions/Aula15-Copia/Contact_Us_Steps_FINAL.java
@@ -1,3 +1,4 @@
+/*
 package stepDefinitions;
 
 import io.cucumber.java.After;
@@ -7,14 +8,10 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
-
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
-
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;
-
 
 public class Contact_Us_Steps {
     private WebDriver driver;
@@ -22,29 +19,10 @@ public class Contact_Us_Steps {
     @Before
     public void setup() {
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/java/drivers/chromedriver.exe");
-        // ou System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "c:/selenium/drivers/chromedriver.exe");
-
-/*
-        não funcionou no meu teste.
         ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);*/
-
-        ChromeOptions chromeOptions = new ChromeOptions();
-        //chromeOptions.addArguments("--start-maximized"); - aqui não funcionou no meu teste
-
-        chromeOptions.addArguments("--remote-allow-origins=*");
-        chromeOptions.addArguments("--disable-extensions");
-
-        //chromeOptions.addArguments("test-type");
-        //chromeOptions.addArguments("--start-maximized");
-        chromeOptions.addArguments("--window-size=1024,768");
-        chromeOptions.addArguments("--enable-precise-memory-info");
-        chromeOptions.addArguments("--disable-popup-blocking");
-        chromeOptions.addArguments("--disable-default-apps");
-        chromeOptions.addArguments("--test-type=browser");
-
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
         driver = new ChromeDriver(chromeOptions);
-        //driver.manage().window().maximize();
+        driver.manage().window().maximize();
     }
 
     @After
@@ -57,30 +35,26 @@ public class Contact_Us_Steps {
         driver.get("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
     }
     @When("I enter a first name")
-    public void i_enter_a_first_name()  {
+    public void i_enter_a_first_name() {
         driver.findElement(By.cssSelector("[name=\"first_name\"]")).sendKeys("Joe");
-        //Thread.sleep(3000);
     }
     @And("I enter a last name")
     public void i_enter_a_last_name() {
         driver.findElement(By.cssSelector("[name=\"last_name\"]")).sendKeys("Blogs");
     }
     @And("I enter an email address")
-    public void i_enter_an_email_address(){
-        driver.findElement(By.name("email")).sendKeys("joe_blogs123@gmail.com");
+    public void i_enter_an_email_address() throws InterruptedException {
+        driver.findElement(By.name("email")).sendKeys("joe_blogs123@mail.com");
+        Thread.sleep(3000);
     }
     @And("I enter a comment")
     public void i_enter_a_comment() {
-        driver.findElement(By.name("message")).sendKeys("Hello how are you?");
     }
     @And("I click on the submit button")
     public void i_click_on_the_submit_button() {
-        driver.findElement(By.cssSelector("[value=\"SUBMIT\"]")).click();
     }
     @Then("I should be presented with a successful contact us submission message")
     public void i_should_be_presented_with_a_successful_contact_us_submission_message() {
-        final String MESSAGE_SUBMISSION = "//*[@id='contact_reply']";
-        WebElement messageSubmission = driver.findElement(By.xpath(MESSAGE_SUBMISSION.toString()));
-        Assert.assertEquals(messageSubmission.getText(),"Thank You for your Message!");
     }
 }
+*/
---
+++ src/main/java/stepDefinitions/Aula15-Copia/Contact_Us_Steps_ORIG.java
@@ -0,0 +1,54 @@
+/*
+package stepDefinitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Contact_Us_Steps {
+    private WebDriver driver;
+
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/java/drivers/chromedriver.exe");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @Given("I access the webdriver university contact us page")
+    public void i_access_the_webdriver_university_contact_us_page() {
+    }
+    @When("I enter a first name")
+    public void i_enter_a_first_name() {
+    }
+    @And("I enter a last name")
+    public void i_enter_a_last_name() {
+    }
+    @And("I enter an email address")
+    public void i_enter_an_email_address() {
+    }
+    @And("I enter a comment")
+    public void i_enter_a_comment() {
+    }
+    @And("I click on the submit button")
+    public void i_click_on_the_submit_button() {
+    }
+    @Then("I should be presented with a successful contact us submission message")
+    public void i_should_be_presented_with_a_successful_contact_us_submission_message() {
+    }
+}
+*/
